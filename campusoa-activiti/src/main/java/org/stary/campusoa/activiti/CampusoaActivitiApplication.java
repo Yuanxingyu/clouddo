4fb81212f20eb3efe46fc1bcfc7da7672833a9ba
stary <3303521941@qq.com>
2019-04-15 23:03:56 +0800

    依赖修改

--- campusoa-activiti/src/main/java/org/stary/campusoa/activiti/CampusoaActivitiApplication.java
+++ campusoa-activiti/src/main/java/org/stary/campusoa/activiti/CampusoaActivitiApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableFeignClients(basePackages = {"com.bootdo"})
+@EnableFeignClients(basePackages = {"org.stary.campusoa","com.bootdo"})
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"org.stary.campusoa.activiti", "com.bootdo.clouddocommon"})
